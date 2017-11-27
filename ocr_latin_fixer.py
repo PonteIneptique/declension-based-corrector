823d1866891c08b2d6ad5d6728402810681847b8
Thibault Clérice <leponteineptique@gmail.com>
2017-11-27 15:34:38 +0100

    Adding line cut prevention

--- ocr_latin_fixer.py
+++ ocr_latin_fixer.py
@@ -8,6 +8,17 @@ _match_words = re.compile("\W")
 _match_numbers = re.compile("\D")
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class Corrector:
     """ Declension-Rule Based OCR correct
 
@@ -30,6 +41,7 @@ class Corrector:
                 self.lemmatiseur.compile()
         self.changes = changes
         self.count_changes = 0
+        self.cutting = True
 
     def read_file(self, path: str):
         """ Read the current file
@@ -89,26 +101,57 @@ class Corrector:
         """
         words = _match_tags.sub("", fulltext)
         words = [w for w in _match_words.split(words) if w]
+        words_next = words[1:] + [""]
         replacements = {}
-        for lemmatisation, form in zip(self.lemmatiseur.lemmatise_multiple(" ".join(words)), words):
+        curr_remove = False
+        for lemmatisation, form, next_tok \
+            in zip(self.lemmatiseur.lemmatise_multiple(" ".join(words)), words, words_next):
             key = Corrector.regexp_pattern(form)
-            if len(lemmatisation) == 0 and key not in replacements and _match_numbers.match(form):
+
+            if curr_remove:
+                replacements[key] = \
+                    "\g<1><choice cert=\"high\" source=\"OCR-LINECUT\"><sic>\g<2></sic><corr></corr></choice>\g<3>"
+                curr_remove = False
+
+            elif len(lemmatisation) == 0 and key not in replacements and _match_numbers.match(form):
 
                 proposed = self.propose_changes(form, []+self.changes)
                 if proposed:
                     if len(proposed) == 1:
-                        print("Changing " + form + " to " + proposed[0])
+                        print(bcolors.OKGREEN + "Changing " + form + " to " + proposed[0])
                     elif len(proposed) > 1:
-                        print("Too much choice for " + form + " : " + ", ".join(proposed))
+                        print(bcolors.OKGREEN + "Too much choice for " + form + " : " + ", ".join(proposed))
 
                     replacements[key] = \
-                        "\g<1><choice><sic>\g<2></sic>{}</choice>\g<3>".format(
+                        "\g<1><choice cert=\"medium\" source=\"OCR-CHARACTER-SWAP\">><sic>\g<2></sic>{}</choice>\g<3>".format(
                             "".join(["<corr>{}</corr>".format(cor) for cor in proposed])
                         )
                     self.count_changes += 1
+
+                elif len(self.lemmatiseur.lemmatise_multiple(form + next_tok)[0]) > 0:
+                    print(bcolors.UNDERLINE + "Gluing " + form + " to " + form + next_tok)
+                    replacements[key] = \
+                        "\g<1><choice cert=\"high\" source=\"OCR-LINECUT\">><sic>\g<2></sic><corr>{}</corr></choice>\g<3>".format(
+                            form+next_tok
+                        )
+                    curr_remove = True
+                    self.count_changes += 1
+                elif self.cutting <= len(form):
+                    proposals = self.cut_word(form)
+                    if len(proposals):
+                        replacements[key] = \
+                            "\g<1><choice cert=\"low\" source=\"OCR-AGGLUTINATION\"><sic>\g<2></sic>{}</choice>\g<3>".format(
+                                "".join(["<corr>{}</corr>".format(cor) for cor in proposals])
+                            )
+
+                        self.count_changes += 1
+                        print(bcolors.OKBLUE + form + " was agglutinated : " + ", ".join(proposals))
+                    else:
+                        replacements[key] = "\g<1>\g<2>\g<3>"
+                        print(bcolors.FAIL + form + " not recognized")
                 else:
                     replacements[key] = "\g<1>\g<2>\g<3>"
-                    print(form + " not recognized")
+                    print(bcolors.FAIL + form + " not recognized")
 
         return replacements
 
@@ -161,12 +204,52 @@ class Corrector:
 
         return original_file
 
+    def subwords(self, form: str):
+        """ Tries to find for a given token possible cuts
+
+        :param form: Original token
+        :type form: str
+        :return: List of possible match
+        """
+        if len(form) == 0:
+            return []
+
+        proposals = []
+        for i in range(1, len(form)):
+            start, end = form[:i], form[i:]
+            exists = [len(analysis) > 0 for analysis in self.lemmatiseur.lemmatise_multiple(start+" "+end)]
+            if False not in exists:
+                proposals.append(start+" "+end)
+            if exists[0] is True and exists[1] is False:
+                proposals += [start+" "+subproposal for subproposal in self.subwords(end)]
+
+        return proposals
+
+    def cut_word(self, form: str):
+        """ Tries to find for a given token possible cuts
+
+        :param form: Original token
+        :type form: str
+        :return: List of possible match
+        """
+        proposals = sorted(self.subwords(form), key=lambda x: len(x.split()))
+        if len(proposals) == 0:
+            return []
+        minimal_len = len(proposals[0].split())
+        return [p for p in proposals if len(p.split()) == minimal_len]
+
 if __name__ == "__main__":
     corrector = Corrector(changes=[
         ("s", "a"),
         ("o", "c"),
-        ("e", "c")
+        ("c", "o"),
+        ("e", "c"),
+        ("l", "I")
     ])
+    # Test that checks we have not broken things
+    assert corrector.cut_word("quiaadextris") == ["quia a dextris"]
+    corrector.cutting = 5  # Set to false for real output as cutting seems to change too much !
+    # Launch !
     with open("output.xml", "w") as output:
         output.write(corrector.xml_corrector("./full_text.xml"))
     print(str(corrector.count_changes) + " change(s) done")
